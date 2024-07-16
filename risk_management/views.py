87ccf88435af7b3b97e1bcbeca53ec2e7399958d
Ali <aliahadmd1@gmail.com>
2024-07-16 14:31:08 +0600

    template view fix

--- risk_management/views.py
+++ risk_management/views.py
@@ -2,6 +2,7 @@
 
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
